63ac1cd139131f64f7a48b7b1d9539c1a86fa8b7
Henry-Sky <2191976138@qq.com>
2023-10-04 04:00:06 +0000

    update carbot_camera

--- Carbot_Ros/src/carbot_camera/carbot_camera/camera_pub.py
+++ Carbot_Ros/src/carbot_camera/carbot_camera/camera_pub.py
@@ -3,40 +3,35 @@
 
 import rclpy
 from rclpy.node import Node
-import numpy as np
 from sensor_msgs.msg import Image
 import cv2
-from cv_bridge import CvBridge, CvBridgeError
+from cv_bridge import CvBridge
 
 class Camera_Pub(Node):
     def __init__(self, name):
-        super.__init__(name) 
-        # 关于摄像头编号
+        super().__init__(name)
+        # 摄像头编号参数
         self.declare_parameter("cap_id",0)
         self.cap_id = self.get_parameter("cap_id").get_parameter_value().integer_value
         # 创建画面发布者
         self.image_publisher = self.create_publisher(Image, "/image_raw", 2)
         # 打开摄像头
         self.cap = cv2.VideoCapture(self.cap_id)
+        # 创建转换桥(Ros与Opencv)
         self.bridge = CvBridge()
-        self.count = 0
-        
+        # 消息频率
         self.time_period = 0.001
-        self.timer = self.create_timer(time_period, self.timer_callback)
+        self.timer = self.create_timer(self.time_period, self.timer_callback)
         
     def timer_callback(self):
         self.ret = False
         self.ret, self.frame = self.cap.read()
         if self.ret:
-            self.msg = self.bridge.cv2_to_compressed_imgmsg(self.frame, 'jpg')
+            self.msg = self.bridge.cv2_to_imgmsg(self.frame, encoding='passthrough')
             self.image_publisher.publish(self.msg)
     
 def main():
     rclpy.init()
     camera_pub = Camera_Pub('carbot_camera')
     rclpy.spin(camera_pub)
-    rclpy.shutdown() 
-        
-            
-        
-        
+    rclpy.shutdown()      
