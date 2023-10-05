758e91419644aeb633e511bc5b37a4f60b3b19e4
Henry-Sky <2191976138@qq.com>
2023-10-05 03:42:57 +0000

    fix launch

--- Testspace/webcamera/trans.py
+++ Testspace/webcamera/trans.py
@@ -12,17 +12,16 @@ class VideoCamera(object):
     def get_frame(self):
         flag, img = self.cap.read()
         if flag:
-            ret,img_encode = cv2.imencode('.jpg', img)
-            return ret,img_encode.tobytes(),codeinfo
-        else:
-            return False
-    
-# 二维码识别函数
-def qrcheck(img):
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    qrcoder = cv2.QRCodeDetector()
-    codeinfo, points, straight_qrcode = qrcoder.detectAndDecode(gray_img)
-    return codeinfo
+            return img
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        ret, buffer = cv2.imencode('.jpg',frame)
+        frame = buffer.tobytes()  
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
             
 # 网页框架
 app = Flask(__name__)
@@ -31,17 +30,6 @@ app = Flask(__name__)
 @app.route('/') 
 def index():
     return render_template('index.html')
- 
-def gen(camera):
-    while True:
-        flag,frame,codeinfo = camera.get_frame()
-        if codeinfo != "":
-            print("识别二维码:",codeinfo)
-        if flag:
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        else:
-            continue
 
 # 视频页
 @app.route('/video_feed')
